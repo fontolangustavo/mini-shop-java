fd81ff2920e4494a63bd4b8bf907a5f3b0de4906
Leila Sampaio - LSM <leila.sampaio@iteris.com.br>
2023-06-12 14:41:51 -0300

    Spring initial settings

---
+++ src/main/java/br/com/iteris/universidade/minishop/MinishopApplication.java
@@ -0,0 +1,13 @@
+package br.com.iteris.universidade.minishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinishopApplication.class, args);
+	}
+
+}
