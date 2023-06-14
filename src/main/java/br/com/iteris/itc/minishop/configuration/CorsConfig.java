73a7ddd71e96962ede80c79d48898f62529883ed
Leila Sampaio - LSM <leila.sampaio@iteris.com.br>
2023-06-14 10:11:19 -0300

    add Cors Config

---
+++ src/main/java/br/com/iteris/itc/minishop/configuration/CorsConfig.java
@@ -0,0 +1,28 @@
+package br.com.iteris.itc.minishop.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@Profile({ "local", "dev"})
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer (){
+        return new WebMvcConfigurer() {
+            public void addCorsConfigurer (CorsRegistry registry){
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
+}
