4e42561287b520766117722f5680405da2492e5c
Leila Sampaio - LSM <leila.sampaio@iteris.com.br>
2023-06-23 17:54:35 -0300

    add context to tests

--- src/test/java/br/com/iteris/itc/minishop/MinishopApplicationTests.java
+++ src/test/java/br/com/iteris/itc/minishop/MinishopApplicationTests.java
@@ -1,11 +1,12 @@
 package br.com.iteris.itc.minishop;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
 class MinishopApplicationTests {
 
 	@Test
