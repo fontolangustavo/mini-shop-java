a26b04d831acd72595bcea819b2b4bcf6f2e1ef3
Leila Sampaio - LSM <leila.sampaio@iteris.com.br>
2023-06-26 14:33:22 -0300

    fix covarege e unit test configs

---
+++ src/test/java/br/com/iteris/itc/minishop/service/MiniShopServiceTests.java
@@ -0,0 +1,23 @@
+package br.com.iteris.itc.minishop.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+public class MiniShopServiceTests {
+
+    @InjectMocks
+    private MiniShopService service;
+
+    @Test
+    public void foo(){
+        assertEquals ("Api minishop" , service.foo());
+    }
+
+}
