9aba0302a999e52bd349a0dbf197037520a2ccdf
Leila Sampaio - LSM <leila.sampaio@iteris.com.br>
2023-06-27 12:46:39 -0300

    rm import  @datajpatest

--- src/test/java/br/com/iteris/itc/minishop/service/MiniShopServiceTests.java
+++ src/test/java/br/com/iteris/itc/minishop/service/MiniShopServiceTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
