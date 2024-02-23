d763e2a6942be3d0beabf1b655974bf0f6a0fc3f
Fontolan <fontolangustavo@gmail.com>
2024-02-23 14:42:45 -0300

    fix: ensure the data provider doesn't handle exceptions just to fetch product data

--- src/main/java/br/com/iteris/itc/minishop/configuration/RestResponseEntityExceptionHandler.java
+++ src/main/java/br/com/iteris/itc/minishop/configuration/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.iteris.itc.minishop.configuration;
 
 import br.com.iteris.itc.minishop.core.exceptions.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,13 +9,28 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-    public record JsonResponse(int statusCode, String message) {}
+    public record JsonResponse(
+            String timestamp,
+            int status,
+            String error,
+            String message,
+            String path)
+    {}
 
     @ExceptionHandler(value = NotFoundException.class)
-    protected ResponseEntity<JsonResponse> handleNotFound(RuntimeException ex, WebRequest request){
-        JsonResponse json = new JsonResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+    protected ResponseEntity<JsonResponse> handleNotFound(RuntimeException ex, WebRequest request, final HttpServletRequest httpServletRequest){
+        JsonResponse json = new JsonResponse(
+                ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT),
+                HttpStatus.NOT_FOUND.value(),
+                "Not found",
+                ex.getMessage(),
+                httpServletRequest.getRequestURI()
+        );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(json);
     }
--- src/main/java/br/com/iteris/itc/minishop/dataprovider/FindProductByIdImpl.java
+++ src/main/java/br/com/iteris/itc/minishop/dataprovider/FindProductByIdImpl.java
@@ -8,6 +8,7 @@ import br.com.iteris.itc.minishop.dataprovider.repository.mapper.ProductEntityMa
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
@@ -18,13 +19,9 @@ public class FindProductByIdImpl implements FindProductById {
     private final ProductEntityMapper productEntityMapper;
 
     @Override
-    public Product find(String id) {
+    public Optional<Product> find(String id) {
         var productEntity = productRepository.findById(UUID.fromString(id));
 
-        if (productEntity.isEmpty()) {
-            throw new NotFoundException("Product not found");
-        }
-
-        return productEntityMapper.toProduct(productEntity.get());
+        return productEntity.map(productEntityMapper::toProduct);
     }
 }
